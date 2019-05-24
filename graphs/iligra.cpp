af16be38471f53b2f786e560315b5c853fca0e42
kborkowska <ka.borkowska@outlook.com>
2019-05-24 02:47:36 +0200

    sample iligra

--- graphs/iligra.cpp
+++ graphs/iligra.cpp
@@ -13,35 +13,157 @@ Iligra::Iligra(){
 bool Iligra::changeState(){
 
     switch(step){
-          case NONE:          stepInfo = "Can't move to the next step!\n Load a line graph";
-                              return false;
-          case LOADED:        step = ARBITRARY_N1;
-                              stepInfo = "Chose one arbitrary node";
-                              choseArbitraryNode();
-                              return true;
-          case ARBITRARY_N1:  stepInfo = "Chose one of its neighboours";
-                              step = ARBITRARY_N2;
-                              choseArbitraryNeighbour();
-
-
+          case NONE:            stepInfo = "Can't move to the next step!\n Load a line graph";
+                                return false;
+          case LOADED:          step = ARBITRARY_N1;
+                                stepInfo = "Chose one arbitrary node - n1";
+                                choseArbitraryNode();
+                                return true;
+          case ARBITRARY_N1:    stepInfo = "Chose one of its neighboours - n2";
+                                step = ARBITRARY_N2;
+                                choseArbitraryNeighbour();
+                                return true;
+          case ARBITRARY_N2:    stepInfo = "Add two nodes to the reconstructed graph G.";
+                                step = ADD_FIRST_NODES;
+                                addFirstNodes();
+                                return true;
+          case ADD_FIRST_NODES: stepInfo = "Add a link between new nodes G. It corresponds to n1. "
+                                           "Remove n1 from Nw";
+                                step = CONNECT_N1;
+                                connectN1();
+                                return true;
+          case CONNECT_N1:      stepInfo = "To the first node v1 in G add a verticle corresponding to n2. "
+                                           "Move n2 form Nw to Nh";
+                                step = FIRST_CONECT_N2;
+                                firstConnectN2();
+                                return true;
+          case FIRST_CONECT_N2: stepInfo = "Set all neighbours of n1, but not n2, as incident to v2."
+                                           "Set J to all the nodes that are both neighbours to n1 and n2";
+                                step = N1_ONLY_NGBRS;
+                                n1OnlyNeighbours();
+                                return true;
+          case N1_ONLY_NGBRS:   stepInfo = "Analyze size of J.";
+                                step = ANALYSE_J;
+          case ANALYSE_J:       {if(J.size()==1 || J.size()==2){
+                                    stepInfo = "J has 1 or 2 nodes. Check whether one of its nodes has more than 2 neighbours that are not n1 or n1"
+                                               " and all of its neighbours are connected to either n1 or n2.";
+                                    step = ONE_TWO_J;
+                                } else {
+                                    stepInfo = "J has 3 or more nodes. Check whether there is a node"
+                                               " that is not adjecent to any other node in J";
+                                    step = THREE_J;
+                                }
+                                return true;
+                                }
     }
     return true;
 }
 
-bool Iligra::changeStep(Step newStep){
-    step = newStep;
-    return true;
+
+
+
+
+void Iligra::n1OnlyNeighbours(){
+    //Nw  = H.getNodesIndexes \ n1 n2 (n1\n2)
+    //Nh Nb[0] (n1\n2)
+    //Nb Nw[0].adjecencyList()
+    //vlh v1 (v2)
+    //highlighted = Nw[0] (n1\n2)
+    //G -v1-v2
+    // J (n1 n n2)
+    std::vector<int> nb2 = H.getSingleAdjecencyList(Nb[0]);
+    for(std::vector<int>::iterator it = ++Nb.begin(); it<Nb.end(); ++it){
+        std::vector<int>::iterator it2 = nb2.begin();
+        for(; it2<nb2.end(); ++it2){
+            if(*it == *it2){
+                J.push_back(*it);
+                break;
+            }
+        }
+        if(it2 == nb2.end()){
+            Nh.push_back(*it);
+            highlighted.push_back(*it);
+            for(std::vector<int>::iterator itw = Nw.begin(); itw<Nw.end(); ++itw){
+                if(*itw == *it){
+                    Nw.erase(it);
+                    break;
+                }
+            }
+            vlh.push_back(G.getNodesIndexes()[1]);
+        }
+    }
+}
+
+void Iligra::firstConnectN2(){
+    //Nw  = H.getNodesIndexes \ n1 n2
+    //Nh Nb[0]
+    //Nb Nw[0].adjecencyList()
+    //vlh v1
+    //highlighted = Nw[0]
+    //G -v1-v2
+    G.addAdjecentToANode(-10, G.getNodesIndexes()[0]);
+    Nh.push_back(highlighted[1]);
+    vlh.push_back(G.getNodesIndexes()[0]);
+    for(std::vector<int>::iterator it = Nw.begin(); it<Nw.end(); ++it){
+        if(*it == Nh[0]){
+            Nw.erase(it);
+            break;
+        }
+    }
+    return;
+}
+
+void Iligra::connectN1(){
+    //Nw  = H.getNodesIndexes \ Nw[0]
+    //Nh empty
+    //Nb Nw[0].adjecencyList()
+    //vlh empty
+    //highlighted = Nw[0]
+    //G v1-v2
+    G.connect(0,1);
+    Nw.erase(Nw.begin());
+    return;
+}
+
+void Iligra::addFirstNodes(){
+    //Nw  = H.getNodesIndexes
+    //Nh empty
+    //Nb Nw[0].adjecencyList()
+    //vlh empty
+    //highlighted = Nw[0]
+    //G v1 v2
+    G.addNode();
+    G.addNode();
+    return;
+}
+
+int Iligra::choseArbitraryNeighbour(){
+    //Nw  = H.getNodesIndexes
+    //Nh empty
+    //Nb Nw[0].adjecencyList()
+    //vlh empty
+    //highlighted = Nw[0] Nb[0]
+    //G empty
+    highlighted.push_back(Nb[0]);
+    return Nb[0];
 }
 
 int Iligra::choseArbitraryNode(){
+    //Nw  = H.getNodesIndexes
+    //Nh empty
+    //Nb Nw[0].adjecencyList()
+    //vlh empty
+    //highlighted = Nw[0]
+    //G epmty
     highlighted.push_back(Nw[0]);
     Nb = H.getSingleAdjecencyList(Nw[0]);
     return Nw[0];
 }
 
-int Iligra::choseArbitraryNeighbour(){
-    highlighted.push_back(Nb[0]);
-    return Nb[0];
+
+bool Iligra::changeStep(Step newStep){
+    step = newStep;
+    return true;
 }
 
 bool Iligra::loadFromFile(QString file){
--- graphs/iligra.h
+++ graphs/iligra.h
@@ -12,10 +12,14 @@ public:
     Graph H;
     Graph G;
 
-    std::vector<int> Nw;
-    std::vector<int> Nh;
-    std::vector<int> Nb;
+    std::vector<int> Nw; //The set of the nodes in H,
+    //corresponding to thelinks inGwhose incident nodes are not yet determined
+    std::vector<int> Nh; //The set of the nodes inH,
+    //corresponding to thelinks inGof which one incident node is determined
+    std::vector<int> Nb; //The set of the neighbors
+    std::vector<int> J;
 
+    std::vector<int> vlh;
     std::vector<int> highlighted;
 
     QString stepInfo;
@@ -24,13 +28,19 @@ public:
     bool changeState();
     bool loadFromFile(QString file);
 
-    enum Step {NONE, LOADED, ARBITRARY_N1, ARBITRARY_N2};
+    enum Step {NONE, LOADED, ARBITRARY_N1, ARBITRARY_N2, ADD_FIRST_NODES,
+               CONNECT_N1, FIRST_CONECT_N2, N1_ONLY_NGBRS, ANALYSE_J, ONE_TWO_J,
+               THREE_J};
 
 private:
     Step step;
     bool changeStep(Step newStep);
     int choseArbitraryNode();
     int choseArbitraryNeighbour();
+    void addFirstNodes();
+    void connectN1();
+    void firstConnectN2();
+    void n1OnlyNeighbours();
 };
 
 #endif // ILIGRA_H
